dec9a2063fff3c0d8c7f0d087494e65244433514
Votybe <aymmai@gmail.com>
2024-11-14 08:00:59 +0100

    feat: test intellij

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express()
 app.get('/api/test', (req, res) => {
     res.status(200).json({ message: 'Hello SonarQube!' })
 })
+console.log("This is a very long line created to test the ESLint max-line rule and see if it triggers an error because the length of this line is excessively long, with a lot of unnecessary text to make it go over any reasonable limit that would typically be imposed by such a rule. Let's see if it works!");
 
 app.get('/api/quote', async (req, res) => {
     try {
